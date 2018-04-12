22d350526307bc31f9a2b74de53b033d3a8a7d44
Chris Yang <ysm0622@gmail.com>
2018-04-12 17:19:06 +0900

    Init: nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,79 @@
+<template>
+  <!-- <v-app> -->
+    <nuxt/>
+  <!-- </v-app> -->
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+body {
+  height: 100%;
+  color: white;
+  overflow: hidden;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+    'Lucida Sans', Arial, sans-serif;
+  margin: auto;
+  padding: 0;
+  #__nuxt,
+  #__layout {
+    margin: 0;
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    background: #020305;
+    overflow: hidden;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+a {
+  &:link,
+  &:visited,
+  &:hover,
+  &:active {
+    color: white;
+  }
+}
+::-webkit-scrollbar {
+  width: 0px; /* remove scrollbar space */
+  background: transparent; /* optional: just make scrollbar invisible */
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-layout column justify-center align-center>
+    <img class="splash" src="splash1.png">
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+const a = 1
+console.log(a)
+
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  background: #020305;
+}
+</style>
+
